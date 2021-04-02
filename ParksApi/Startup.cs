5f4f69c800623bd2e373bb655ddeeb4bff08643a
Dani Renner <danijrenner@gmail.com>
2021-04-02 08:51:20 -0700

    add program setup

---
+++ ParksApi/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Park.Models;
+using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
+
+namespace Park
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<ParkContext>(opt =>
+        opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Parks API",
+          Description = "A simple example ASP.NET Core Web API",
+          Contact = new OpenApiContact
+          {
+              Name = "Dani Renner",
+              Email = "danijrenner@gmail.com",
+              Url = new Uri("https://www.github.com/dani-renner"),
+          },
+          License = new OpenApiLicense
+          {
+            Name = "Use under MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT"),
+          }
+        });
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+      var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+      c.IncludeXmlComments(xmlPath);
+      });
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      app.UseStaticFiles();
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+      });
+      app.UseRouting();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
