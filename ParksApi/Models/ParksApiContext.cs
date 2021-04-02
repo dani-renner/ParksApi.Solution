fc4b80548683a00f68edf741ef896e772fb4148b
Dani Renner <danijrenner@gmail.com>
2021-04-02 11:15:22 -0700

    rename

---
+++ ParksApi/Models/ParksApiContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksApi.Models
+{
+  public class ParksApiContext : DbContext
+  {
+    public ParksApiContext(DbContextOptions<ParksApiContext> options)
+      : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+      .HasData(
+        new Park { ParkId = 1, Name = "Yellowstone", SqMiles = 3471, Location = "Wyoming, Montana, and Idaho" },
+        new Park { ParkId = 2, Name = "Niagara Falls National Heritage Area", SqMiles = 1, Location = "Niagara Falls, Lewiston, and Porter, New York" },
+        new Park { ParkId = 3, Name = "Yosemite National Park", SqMiles = 1169, Location = "California" },
+        new Park { ParkId = 4, Name = "Glacier National Park", SqMiles = 1583, Location = "Montana" },
+        new Park { ParkId = 5, Name = "Frontenac State Park", SqMiles = 4, Location = "Minnesota" }
+      );
+    }
+    public DbSet<Park> Parks { get; set; }
+  }
+}
