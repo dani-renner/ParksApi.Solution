d9a927ce9d9c2f29262abfb04a04a79e9602d265
Dani Renner <danijrenner@gmail.com>
2021-04-02 11:54:51 -0700

    add Initial migrations

---
+++ ParksApi/Migrations/20210402185205_Initial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    SqMiles = table.Column<int>(type: "int", nullable: false),
+                    Location = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Location", "Name", "SqMiles" },
+                values: new object[,]
+                {
+                    { 1, "Wyoming, Montana, and Idaho", "Yellowstone", 3471 },
+                    { 2, "Niagara Falls, Lewiston, and Porter, New York", "Niagara Falls National Heritage Area", 1 },
+                    { 3, "California", "Yosemite National Park", 1169 },
+                    { 4, "Montana", "Glacier National Park", 1583 },
+                    { 5, "Minnesota", "Frontenac State Park", 4 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
